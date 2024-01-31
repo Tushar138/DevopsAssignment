2114ea56e97fff679f35a34d99637992262d77c8
tushar01 <tushar01@nagarro.com>
2024-01-31 19:13:30 +0530

    Change Java version from 17 to 11

--- src/main/java/com/training/nagarro/devops/controller/HomeController.java
+++ src/main/java/com/training/nagarro/devops/controller/HomeController.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
